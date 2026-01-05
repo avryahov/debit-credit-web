3f933c1860c77af689459de9039ae0fe89663561
avrjakhov <a.v.rjakhov@yandex.ru>
2026-01-05 16:20:11 +0500

    feat: app-flow — connect Launcher to App.jsx, add transition logic

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Launcher } from '../features/launcher';
+import { PinScreen } from '../pages/pin-screen';
 import './App.css';
 
 function App() {
+  const [showLauncher, setShowLauncher] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLauncher(false);
+    }, 1500); // 1.5 секунды
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Debit & Credit</h1>
-        <p>Приложение запущено.</p>
-      </header>
-    </div>
+    <div className="App">{showLauncher ? <Launcher /> : <PinScreen />}</div>
   );
 }
 
